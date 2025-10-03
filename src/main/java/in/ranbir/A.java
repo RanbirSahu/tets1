000d69d9758e474a1f02477fb32d7f4ac501d4f9
Ranbir Sahu <ranbirsahu3@gmail.com>
2025-10-03 11:12:02 +0530

    3

--- src/main/java/in/ranbir/A.java
+++ src/main/java/in/ranbir/A.java
@@ -4,10 +4,20 @@ public class A {
 
   int x=1000;
 
+<<<<<<< Updated upstream
 
 ############################Then i jump in to another branch and worked on that branch in important requirment
 do this task 100 %  and push it
  
+=======
+ ==============let say i worked on some like login features my work is done 100 %===================then my TL said one another important requirment come please do this first the do this later
+so i cant upload this code that is 75% is done 
+so i am doing git stash and git automatically hiding this code from my staging/working tree for temporarily bases so i can come back later and do a git stash apply and worked on that
+
+###################################################################################################################################################
+
+
+>>>>>>> Stashed changes
 MadanPur
 ooty
 odisha
